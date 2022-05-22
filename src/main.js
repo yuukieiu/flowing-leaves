8fb6bbb2c430407c37323258007f1ecf1076434b
yuuki <yuukieiu@gmail.com>
2022-05-22 18:46:01 +0900

    ビルドエラーを修正（7:8  error  Identifier 'aws_exports'...）

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { Amplify } from 'aws-amplify'
-import aws_exports from './aws-exports'
+import AwsExports from './aws-exports'
 import {
   applyPolyfills,
   defineCustomElements
 } from '@aws-amplify/ui-components/loader'
-Amplify.configure(aws_exports)
+Amplify.configure(AwsExports)
 applyPolyfills().then(() => {
   defineCustomElements(window)
 })
